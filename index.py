6f971fff3882b208aa2f337b810fd6bfee35afb1
viraj uk <virajk@saksglobal.com>
2020-12-14 01:41:42 -0500

    coding cleaning

--- index.py
+++ index.py
@@ -1,36 +1,9 @@
-from utils.swap_test import SWAP_T
-
-# img2 = 'images/dadario.jpg'
-# img1 = 'images/emma.jpg'
+from utils.swap import SWAP
 
 img1 = 'images/chris-hems.jpg'
 img2 = 'images/tom-2.jpg'
 
-SWAP_T.enable_visualization()
-swap = SWAP_T(img1, img2)
+swap = SWAP(img1, img2)
 
-SWAP_T.enable_visualization(False)
-swap.first_face_landmarks()
-
-SWAP_T.enable_visualization(False)
-swap.second_face_landmarks()
-
-SWAP_T.enable_visualization(False)
-swap.first_convex_hull()
-
-SWAP_T.enable_visualization(False)
-swap.second_convex_hull()
-
-SWAP_T.enable_visualization(False)
-swap.delaunay_triangle_1()
-
-SWAP_T.enable_visualization(False)
-swap.delaunay_triangle_2()
-
-SWAP_T.enable_visualization(False)
-swap.combine_triangle()
-
-SWAP_T.enable_visualization(False)
-swap.final_show()
-
+swap.swap()
 swap.show_images()
