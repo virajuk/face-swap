8036a9fd526af226a7ba98cdf1f523ba23d104d9
viraj uk <virajk@saksglobal.com>
2020-12-14 05:30:33 -0500

    cleaning images

--- index.py
+++ index.py
@@ -2,11 +2,14 @@ from utils.swap import SWAP
 
 # both images needs to be exact same resolution
 
-img1 = 'images/anna.jpg'
-img2 = 'images/lawrence-2-scaled.jpg'
+# img1 = 'images/anna.jpg'
+# img2 = 'images/lawrence-2-scaled.jpg'
 
 # img1 = 'images/chris-hems.jpg'
 # img2 = 'images/tom-2.jpg'
+
+img1 = 'images/annabelle-wallis-600.jpg'
+img2 = 'images/elsa.jpg'
 
 swap = SWAP(img1, img2)
 
